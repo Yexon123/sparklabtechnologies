71702b9f1089fdd739801b67410bf0b3475e25d5
Yexon123 <9yexon27@gmail.com>
2025-07-26 02:15:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Coloca el año actual automáticamente en el footer
+document.addEventListener('DOMContentLoaded', () => {
+  const yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
